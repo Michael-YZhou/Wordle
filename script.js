a72ac3a3b6fe29e292d14ef5d32667d448e1605c
Yang Zhou <yangzhouinter@gmail.com>
2023-03-31 01:12:06 +1100

    added missing breaks to the swith statement in section 3

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function checkResult(row, inputWord) {
         correctLetterId < maxLen;
         correctLetterId++
       ) {
-        // boxId start from 1, however the for-loop index starts from 0,
+        // nth-child() start from 1, however the for-loop index starts from 0,
         // Therefore add 1 to index before pass it into querySelector.
         let box = document.querySelector(
           `.row-${row} div:nth-child(${inputLetterId + 1})`
@@ -56,12 +56,10 @@ function checkResult(row, inputWord) {
   if (round === 6) {
     console.log("You lose. Try again!");
     play = false;
-    // return;
   }
 
   console.log("\n");
 }
-// checkResult("AARTI");
 
 // ******************************** Create keyboard ********************************
 
@@ -116,7 +114,7 @@ function displayLetter(rowId) {
   let keys = document.getElementsByClassName("key");
   for (let keyElement of keys) {
     let key = keyElement.textContent;
-    keyElement.addEventListener("click", function handleClick() {
+    keyElement.addEventListener("click", function () {
       switch (key) {
         case "Enter":
           // click Enter increases the rowId by 1, which moves the cursor to the next line;
@@ -125,7 +123,7 @@ function displayLetter(rowId) {
           // apply bundry to the row (0-5)
           console.log("play:", play);
           if (rowId < 6 && play) {
-            if (boxId === 5) {
+            if (validWords.includes(curLetter) && boxId === 5) {
               inputWord = curLetter;
               boxId = 0;
               // console.log(rowId);
@@ -137,76 +135,74 @@ function displayLetter(rowId) {
               // reset the user input for the next round
               curLetter = "";
             } else {
-              console.log("Must enter all 5 letters");
+              // word is not valid, or less than 5 letters
+              console.log("Word is not valid");
             }
           } else {
             // when user has played 6 rounds or have won the game.
             console.log("Please start a new game!");
           }
-        // break;
+          break;
         case "Backspace":
           // apply bundry to the row (0-5)
           if (rowId < 6) {
             // if don't specify this condition, Backspace will be triggered when press Enter
-            if (key !== "Enter") {
-              if (boxId === 0 || (boxId > 5 && boxId % 5 === 0)) {
-                console.log("nothing to delete");
-                console.log(boxId);
-              } else {
-                // select the box using :nth-child()
-                output = document.querySelector(
-                  `.row-${rowId} div:nth-child(${boxId}) span`
-                );
-                // console.log(output);
-                // console.log(boxId);
-                output.textContent = "";
-                curLetter = curLetter.slice(0, curLetter.length - 1);
-                console.log(curLetter);
-                // console.log(boxId);
-                // add css class to the letter box when letter deleted
-                outputBox = document.querySelector(
-                  `.row-${rowId} div:nth-child(${boxId})`
-                );
-                outputBox.classList.remove("letter-uncheck");
-                // update cursor
-                boxId--;
-                break;
-              }
+            if (boxId === 0 || (boxId > 5 && boxId % 5 === 0)) {
+              console.log("nothing to delete");
+              console.log(boxId);
+            } else {
+              // select the box using :nth-child()
+              output = document.querySelector(
+                `.row-${rowId} div:nth-child(${boxId}) span`
+              );
+              // console.log(output);
+              // console.log(boxId);
+              output.textContent = "";
+              curLetter = curLetter.slice(0, curLetter.length - 1);
+              console.log("current input:", curLetter);
+              // console.log(boxId);
+              // add css class to the letter box when letter deleted
+              outputBox = document.querySelector(
+                `.row-${rowId} div:nth-child(${boxId})`
+              );
+              outputBox.classList.remove("letter-uncheck");
+              // update cursor
+              boxId--;
             }
           } else {
             // when user has played 6 rounds or have won the game.
             console.log("Please start a new game!");
           }
+          break;
         default:
           if (rowId < 6 && play) {
             // if don't specify this condition, Backspace & Enter will also be triggered when press any key
-            if (key !== "Backspace" && key !== "Enter") {
-              if (boxId !== 0 && boxId % 5 === 0) {
-                console.log("exceed maximun letter");
-              } else {
-                // update cursor
-                boxId++;
-                // console.log(boxId);
-                // select the box using :nth-child()
-                output = document.querySelector(
-                  `.row-${rowId} div:nth-child(${boxId}) span`
-                );
-                // console.log(output);
-                output.textContent = key;
-                // console.log(key);
-                curLetter += key;
-                console.log(curLetter);
-                // add css class to the letter box when letter entered
-                outputBox = document.querySelector(
-                  `.row-${rowId} div:nth-child(${boxId})`
-                );
-                outputBox.classList.add("letter-uncheck");
-              }
+            if (boxId !== 0 && boxId % 5 === 0) {
+              console.log("exceed maximun letter");
+            } else {
+              // update cursor
+              boxId++;
+              // console.log(boxId);
+              // select the box using :nth-child()
+              output = document.querySelector(
+                `.row-${rowId} div:nth-child(${boxId}) span`
+              );
+              // console.log(output);
+              output.textContent = key;
+              // console.log(key);
+              curLetter += key;
+              console.log("current input:", curLetter);
+              // add css class to the letter box when letter entered
+              outputBox = document.querySelector(
+                `.row-${rowId} div:nth-child(${boxId})`
+              );
+              outputBox.classList.add("letter-uncheck");
             }
           } else {
             // when user has played 6 rounds or have won the game.
             console.log("Please start a new game!");
           }
+          break;
       }
     });
   }
